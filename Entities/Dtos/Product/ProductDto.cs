1d60cd9fba446dfd44d866216784b0349a0caaa1
Ali Emre Yeşilyurt <99074640+aliemreyesilyurt@users.noreply.github.com>
2024-10-13 13:18:35 +0300

    ProductDto record structure was created and Create.cshtml page was updated

---
+++ Entities/Dtos/Product/ProductDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Dtos.Product
+{
+    public record ProductDto
+    {
+        public int Id { get; init; }
+
+        [Required(ErrorMessage = "Product name is required.")]
+        public string ProductName { get; init; }
+
+        [Required(ErrorMessage = "Price is required.")]
+        public decimal Price { get; init; }
+        public int? CategoryId { get; init; }
+    }
+}
