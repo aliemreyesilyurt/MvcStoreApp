f134f3b9e4cdb3cb725f4454c5eb099051104c07
Ali Emre Yeşilyurt <99074640+aliemreyesilyurt@users.noreply.github.com>
2024-11-07 11:00:11 +0300

    API layer added to the application

---
+++ Presentation/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Services.Contracts;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IServiceManager _manager;
+
+        public ProductController(IServiceManager manager)
+        {
+            _manager = manager;
+        }
+
+        [HttpGet]
+        public IActionResult GetAllProducts()
+        {
+            var products = _manager.ProductService.GetAllProducts(false);
+
+            return Ok(products);
+        }
+    }
+}
